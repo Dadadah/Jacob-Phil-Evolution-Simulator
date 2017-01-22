d79c4b60dda39918173d5bbc2e53fb11799cfb21
Philip <legoboy1122@yahoo.com>
2017-01-22 01:49:07 -0600

    Added Leg.Avg(A,B)

--- Assets/Scripts/Phil Code/Phil_Leg.cs
+++ Assets/Scripts/Phil Code/Phil_Leg.cs
@@ -4,8 +4,11 @@ using System;
 using UnityEngine;
 
 public class Phil_Leg : MonoBehaviour {
-    Leg me = new Leg();
-
+    private Leg me;
+    public void Start()
+    {
+        me = new Leg();
+    }
 }
 
 class Leg
@@ -27,9 +30,42 @@ class Leg
 
     public Leg()
     {
-        Leg_Curr_Rot =Leg_Start_Rot;
-       // Knee_Curr_Rot = Knee_Start_Rot;
-       // Ankle_Curr_Rot = Ankle_Start_Rot;
+        Leg_Curr_Rot = Leg_Start_Rot;
+        // Knee_Curr_Rot = Knee_Start_Rot;
+        // Ankle_Curr_Rot = Ankle_Start_Rot;
+    }
+
+    public static Leg Avg(Leg A,Leg B)
+    {
+        Leg output = new Leg();
+        output.Upper_Length = (A.Upper_Length + B.Upper_Length) / 2;
+        output.Leg_Start_Rot = Quaternion.Slerp(A.Leg_Start_Rot, B.Leg_Start_Rot, 0.5f);
+        int MoveCount = (A.movements.Length + B.movements.Length) / 2;
+        output.movements = new Move[MoveCount];
+        for (int i = 0; i < MoveCount; i++)
+        {
+            if(i<A.movements.Length && i< B.movements.Length)
+            {
+                output.movements[i] = Move.Avg(A.movements[i], B.movements[i]);
+            }
+            else
+            {
+                if (i < A.movements.Length)
+                {
+                    output.movements[i] = A.movements[i];
+                }
+                if (i < B.movements.Length)
+                {
+                    output.movements[i] = B.movements[i];
+                }
+            }
+        }
+
+        return output;
+    }
+    public void Mutate()
+    {
+        //TODO: See If we will actually need this...
     }
 
 }
